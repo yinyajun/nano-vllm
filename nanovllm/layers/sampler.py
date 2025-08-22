7e2e066b454ee897af816b3f2a9b2967eb229588
yinyajun-hj <yinyajun-hj@huafang.com>
2025-08-22 18:59:37 +0800

    feat: init

--- nanovllm/layers/sampler.py
+++ nanovllm/layers/sampler.py
@@ -15,6 +15,6 @@ class Sampler(nn.Module):
         probs = torch.softmax(logits, dim=-1, dtype=torch.float)
         # logprobs = torch.log_softmax(logits, dim=-1, dtype=torch.float)
         epsilon = 1e-10
-        # gumbel-max sampling trick: 等价于 从softmax分布中采样， 比multinomial高效
+        # gumbel-max trick: 等价于 从softmax分布中采样， 比multinomial高效
         sample_tokens = probs.div_(torch.empty_like(probs).exponential_(1) + epsilon).argmax(dim=-1)
         return torch.where(temperatures == 0, greedy_tokens, sample_tokens)
